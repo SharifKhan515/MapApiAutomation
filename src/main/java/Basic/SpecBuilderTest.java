c928a399a1e6880db95db0c88b9ef6018bf01b2a
SharifKhan515 <sharifkhan515@gmail.com>
2021-07-31 02:33:39 +0600

    1. Created request Spec builder 2. created response spec builder.

--- src/main/java/Basic/SpecBuilderTest.java
+++ src/main/java/Basic/SpecBuilderTest.java
@@ -2,19 +2,24 @@ package Basic;
 
 import files.HelperMethod;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pojo.AddPlace;
 import pojo.Location;
 
-import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
-public class MapAPITest {
+public class SpecBuilderTest {
 
     String place_id;
     String key;
@@ -23,17 +28,18 @@ public class MapAPITest {
     List<String> types;
     Location location;
     JsonPath responseJson;
+    RequestSpecification requestSpecification;
+    ResponseSpecification responseSpecification;
 
     @BeforeClass
-    public void init(){
-        RestAssured.baseURI = "https://rahulshettyacademy.com";
+    public void init() {
         key = "qaclick123";
         content_type = "application/json";
         addPlace = new AddPlace();
         types = new ArrayList<>();
         types.add("First types");
         types.add("Second types");
-        types.add("Thirs Types");
+        types.add("Third Types");
         location = new Location();
         location.setLat(-50.2369974);
         location.setLng(100.24455445);
@@ -44,19 +50,20 @@ public class MapAPITest {
         addPlace.setPhone_number("1236588789865");
         addPlace.setTypes(types);
         addPlace.setLocation(location);
+        requestSpecification = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addQueryParam("key", key)
+                .setContentType(ContentType.JSON).build();
+        responseSpecification = new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
     }
 
 
-
     @Test
-    public void getPlaceApiTest(){
+    public void getPlaceApiTest() {
 
-       String response  = given().queryParam("key",key).header("Content-Type",content_type )
-                .body(addPlace).when().post("maps/api/place/add/json")
-                .then().assertThat().statusCode(200).extract().response().asString();
+        RequestSpecification request = given().spec(requestSpecification).body(addPlace);
+        Response response = request.when().post("maps/api/place/add/json").then().spec(responseSpecification).extract().response();
 
-       responseJson = HelperMethod.stringToJson(response);
-       place_id = responseJson.getString("place_id");
+        responseJson = HelperMethod.stringToJson(response.asString());
+        place_id = responseJson.getString("place_id");
         System.out.println(place_id);
 
     }
